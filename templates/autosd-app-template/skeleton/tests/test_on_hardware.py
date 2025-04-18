9d58ff6592cf92bfd4c327969a24f382c58387ec
Nick Cao <nickcao@nichi.co>
2025-04-18 12:15:00 -0400

    Improve test script

--- templates/autosd-app-template/skeleton/tests/test_on_hardware.py
+++ templates/autosd-app-template/skeleton/tests/test_on_hardware.py
@@ -17,18 +17,31 @@ class TestOnHardware(JumpstarterTest):
         with client.console.pexpect() as console:
             console.logfile_read = sys.stdout.buffer
             console.expect_exact("login:", timeout=120)
-            console.sendline(
-                client.qemu.username
-            )  # this breaks the hardware abstraction, we need a constant
+            console.sendline("root")
             console.expect_exact("Password:", timeout=10)
-            console.sendline(client.qemu.password)  # same here!
-            console.expect_exact("]$", timeout=10)
+            console.sendline("password")
+            console.expect_exact("]#", timeout=10)
             console.sendline("uname -a")
-            console.expect_exact("]$", timeout=10)
+            console.expect_exact("]#", timeout=10)
             print(console.before.decode())
 
+            console.sendline("podman images")
+            console.expect_exact("]#", timeout=10)
+            assert "localhost/app" in console.before.decode()
+
             console.sendline("systemctl status --no-pager radio.service")
-            console.expect_exact("]$", timeout=10)
-            # console.sendline("podman images")
-            # console.expect_exact("]$", timeout=10)
-            # assert "localhost/app" in console.before.decode()
+            console.expect_exact("]#", timeout=10)
+
+            console.sendline("podman exec -i systemd-radio radio-client")
+            console.expect_exact("Connecting to radio service", timeout=10)
+
+            # test changing volume
+            console.sendline("+" * 10)
+            console.expect_exact("Volume:  [#####################]", timeout=10)
+            console.sendline("-" * 10)
+            console.expect_exact("Volume:  [                     ]", timeout=10)
+            console.sendline("+" * 5)
+            console.expect_exact("Volume:  [##########           ]", timeout=10)
+
+            console.sendline("q")
+            console.expect_exact("]#", timeout=10)
